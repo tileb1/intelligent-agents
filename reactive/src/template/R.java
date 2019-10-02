fc9c8e957ac4a51098b2c8c40bfb95fcd6760a79
Timur <timur.lavrov@epfl.ch>
2019-10-02 23:10:45 +0200

    R

--- reactive/src/template/R.java
+++ reactive/src/template/R.java
@@ -1,10 +1,35 @@
 package template;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import logist.agent.Agent;
+import logist.task.TaskDistribution;
+import logist.topology.Topology;
 import logist.topology.Topology.City;
 
 public class R {
 
+	private HashMap<State, HashMap<City, Double>> rewards;
+	private ArrayList<State> states;
+	
+	public R(HashMap<City, ArrayList<State>> cityStates, Agent agent, Topology topology, TaskDistribution td) {
+		for(ArrayList<State> batchStates : cityStates.values()) {
+			states.addAll(batchStates);
+		}
+		for(State initState : states) {
+			HashMap<City, Double> fixedOriginStateRewards = new HashMap<City, Double>();
+			for (City nextCity : cityStates.keySet()) {
+				if(initState.toCity == nextCity) {
+					fixedOriginStateRewards.put(nextCity, td.reward(initState.fromCity, nextCity) - initState.fromCity.distanceTo(nextCity) * agent.vehicles().get(0).costPerKm());
+				} else if(initState.fromCity.hasNeighbor(nextCity)) {
+					fixedOriginStateRewards.put(nextCity, - initState.fromCity.distanceTo(nextCity) * agent.vehicles().get(0).costPerKm());
+				}
+			}
+			rewards.put(initState, fixedOriginStateRewards);
+		}
+	}
 	public double get(State s, City a) {
-		return 0.0;
+		return rewards.get(s).get(a);
 	}
 }
