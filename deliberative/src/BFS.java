561e7f0a9b637efaa0d3aff14fdb6eb177fa9edb
Tim <tim.lebailly@gmail.com>
2019-10-16 23:49:44 +0200

    sort of working

--- deliberative/src/BFS.java
+++ deliberative/src/BFS.java
@@ -10,78 +10,88 @@ import logist.topology.Topology.City;
 
 public class BFS {
 	public BFS() {
-		
+
 	}
+
 	HashMap<State, State> exploredStates = new HashMap<State, State>();
+
 	public Plan computePlan(State initState) {
 		final long startTime = System.nanoTime();
-		
+
 		LinkedList<State> Q = new LinkedList<State>();
 		State bestGoalState = null;
-		
+
 		Q.add(initState);
-		
+
 		while (!Q.isEmpty()) {
 			State n = Q.poll();
+			
+			if (this.exploredStates.containsKey(n.getParent())) {
+				n.setParent(this.exploredStates.get(n.getParent()));
+			}
+
+			if (!this.exploredStates.containsKey(n)) {
+				this.exploredStates.put(n, n);
+				Q.addAll(n.getChildren());
+
+			} else {
+				if (this.exploredStates.get(n).getCost() > n.getCost()) {
+					this.exploredStates.put(n, n);
+				}
+			}
 			if (n.getIsGoal()) {
 				if ((bestGoalState == null) || (n.getCost() < bestGoalState.getCost())) {
 					bestGoalState = n;
 				}
-				exploredStates.put(n, n);
-			} else if (exploredStates.containsKey(n)) {
-				if (exploredStates.get(n).getCost() > n.getCost()) {
-					exploredStates.put(n, n);	
-				}
-			} else {
-				ArrayList<State> childNodes = n.getChildren();
-				Q.addAll(childNodes);
-				exploredStates.put(n, n);
-			}		
+			}
 		}
+
 		final long timeBegin = System.nanoTime();
-		Plan plan = getPlan(bestGoalState);
+		Plan plan = bestGoalState.getPlan();
 		System.out.println(TimeUnit.MILLISECONDS.convert(timeBegin - startTime, TimeUnit.NANOSECONDS));
 		System.out.println(TimeUnit.MILLISECONDS.convert(System.nanoTime() - timeBegin, TimeUnit.NANOSECONDS));
-		return plan;	
+		return plan;
 	}
-	
-	public Plan getPlan(State state) {
-		State realState = this.exploredStates.get(state);
-		if (realState.getParent() == null) {
-			return new Plan(realState.getCity());
-		}
-		Plan currentPlan = this.getPlan(realState.getParent());
-		
-		TaskSet oneElemMaxLoaded;
-		TaskSet oneElemMaxAvailable = realState.getParent().getAvailableTasks().clone();
-		oneElemMaxAvailable.removeAll(realState.getAvailableTasks());
-		
-		// The current city is a deliver city for the parent node
-		if (oneElemMaxAvailable.size() == 0) {
-			for (City city : realState.getParent().getCity().pathTo(realState.getCity())) {
-				currentPlan.appendMove(city);
-			}
-			// If the current city is a deliver city, then, the parent node has 1 extra loaded task than the current node
-			oneElemMaxLoaded = realState.getParent().getLoadedTasks().clone();
-			oneElemMaxLoaded.removeAll(realState.getLoadedTasks());
-			for (Task onlyTask : oneElemMaxLoaded) {
-				currentPlan.appendDelivery(onlyTask);
-			}
-		}
-		
-		// The current city is a pickup city for the parent node
-		else {
-			for (City city : realState.getParent().getCity().pathTo(realState.getCity())) {
-				currentPlan.appendMove(city);
-			}
-			// If the current city is a pickup city, then, the current node has 1 extra loaded task than the parent
-			oneElemMaxLoaded = realState.getLoadedTasks().clone();
-			oneElemMaxLoaded.removeAll(realState.getParent().getLoadedTasks());
-			for (Task onlyTask : oneElemMaxLoaded) {
-				currentPlan.appendPickup(onlyTask);
-			}
-		}
-//		System.out.println(System.nanoTime() - timeBegin);
-		return currentPlan;
-	}
+
+//	public Plan getPlan(State state) {
+//		State realState = this.exploredStates.get(state);
+//		if (realState.getParent() == null) {
+//			return new Plan(realState.getCity());
+//		}
+//		Plan currentPlan = this.getPlan(realState.getParent());
+//
+//		TaskSet oneElemMaxLoaded;
+//		TaskSet oneElemMaxAvailable = realState.getParent().getAvailableTasks().clone();
+//		oneElemMaxAvailable.removeAll(realState.getAvailableTasks());
+//
+//		// The current city is a deliver city for the parent node
+//		if (oneElemMaxAvailable.size() == 0) {
+//			for (City city : realState.getParent().getCity().pathTo(realState.getCity())) {
+//				currentPlan.appendMove(city);
+//			}
+//			// If the current city is a deliver city, then, the parent node has 1 extra
+//			// loaded task than the current node
+//			oneElemMaxLoaded = realState.getParent().getLoadedTasks().clone();
+//			oneElemMaxLoaded.removeAll(realState.getLoadedTasks());
+//			for (Task onlyTask : oneElemMaxLoaded) {
+//				currentPlan.appendDelivery(onlyTask);
+//			}
+//		}
+//
+//		// The current city is a pickup city for the parent node
+//		else {
+//			for (City city : realState.getParent().getCity().pathTo(realState.getCity())) {
+//				currentPlan.appendMove(city);
+//			}
+//			// If the current city is a pickup city, then, the current node has 1 extra
+//			// loaded task than the parent
+//			oneElemMaxLoaded = realState.getLoadedTasks().clone();
+//			oneElemMaxLoaded.removeAll(realState.getParent().getLoadedTasks());
+//			for (Task onlyTask : oneElemMaxLoaded) {
+//				currentPlan.appendPickup(onlyTask);
+//			}
+//		}
+////		System.out.println(System.nanoTime() - timeBegin);
+//		return currentPlan;
+//	}
 }
--- deliberative/src/State.java
+++ deliberative/src/State.java
@@ -14,6 +14,8 @@ public class State {
 	private TaskSet loadedTasks;
 	private State parent;
 	private double cost;
+	private double costParent;
+	private double costDistance;
 	private int capacityLeft;
 	
 	private Vehicle vehicle;
@@ -26,11 +28,13 @@ public class State {
 		this.vehicle = vehicle;
 
 		if (parent != null) {
-			this.cost = this.parent.getCost() + this.parent.getCity().distanceTo(this.city) * vehicle.costPerKm();
+			this.costParent = this.parent.getCost();
+			this.costDistance = this.parent.getCity().distanceTo(this.city) * vehicle.costPerKm();
 		} else {
-			this.cost = 0;
+			this.costDistance = 0;
+			this.costParent = 0;
 		}
-		this.cost = 0;
+//		this.cost = 0;
 		
 		// We could precompute this to make it faster but this is more user friendly
 		this.capacityLeft = capacityLeft;//vehicle.capacity();
@@ -47,7 +51,10 @@ public class State {
 
 	// -------------------- GETTERS -------------------------
 	public double getCost() {
-		return this.cost;
+		if (this.getParent() == null) {
+			return 0;
+		}
+		return this.costDistance + this.parent.getCost();
 	}
 
 	public TaskSet getAvailableTasks() {
@@ -70,22 +77,56 @@ public class State {
 		return this.loadedTasks.size() == 0 & this.availableTasks.size() == 0;
 	}
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) {
-			return true;
-		}
-		if (o == null || getClass() != o.getClass()) {
-			return false;
-		}
-		State other = (State) o;
-		return other.getLoadedTasks().equals(this.loadedTasks) & other.getAvailableTasks().equals(this.availableTasks)
-				& this.city.equals(other.getCity());
-	}
+//	@Override
+//	public boolean equals(Object o) {
+//		if (this == o) {
+//			return true;
+//		}
+//		if (o == null || getClass() != o.getClass()) {
+//			return false;
+//		}
+//		State other = (State) o;
+//		return other.getLoadedTasks().equals(this.loadedTasks) & other.getAvailableTasks().equals(this.availableTasks)
+//				& this.city.equals(other.getCity());
+//	}
 	
+//	@Override
+//	public int hashCode() {
+//		return Objects.hash(this.availableTasks, this.loadedTasks, this.city);
+//	}
+
 	@Override
 	public int hashCode() {
-		return Objects.hash(this.availableTasks, this.loadedTasks, this.city);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((availableTasks == null) ? 0 : availableTasks.hashCode());
+		result = prime * result + capacityLeft;
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		State other = (State) obj;
+		if (availableTasks == null) {
+			if (other.availableTasks != null)
+				return false;
+		} else if (!availableTasks.equals(other.availableTasks))
+			return false;
+		if (capacityLeft != other.capacityLeft)
+			return false;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		return true;
 	}
 
 	/*
@@ -118,47 +159,57 @@ public class State {
 		return children;
 	}
 	
-//	/*
-//	 * Returns the plan from the initial state to the current state
-//	 */
-//	public Plan getPlan() {
-////		long timeBegin = System.nanoTime();
-//		if (this.parent == null) {
-//			return new Plan(this.city);
-//		}
-//		// Recursive call to parent getPlan() method
-//		Plan currentPlan = this.parent.getPlan();
-//		
-//		TaskSet oneElemMaxLoaded;
-//		TaskSet oneElemMaxAvailable = this.parent.getAvailableTasks().clone();
-//		oneElemMaxAvailable.removeAll(this.availableTasks);
-//		
-//		// The current city is a deliver city for the parent node
-//		if (oneElemMaxAvailable.size() == 0) {
-//			for (City city : this.parent.getCity().pathTo(this.city)) {
-//				currentPlan.appendMove(city);
-//			}
-//			// If the current city is a deliver city, then, the parent node has 1 extra loaded task than the current node
-//			oneElemMaxLoaded = this.parent.getLoadedTasks().clone();
-//			oneElemMaxLoaded.removeAll(this.loadedTasks);
-//			for (Task onlyTask : oneElemMaxLoaded) {
-//				currentPlan.appendDelivery(onlyTask);
-//			}
-//		}
-//		
-//		// The current city is a pickup city for the parent node
-//		else {
-//			for (City city : this.parent.getCity().pathTo(this.city)) {
-//				currentPlan.appendMove(city);
-//			}
-//			// If the current city is a pickup city, then, the current node has 1 extra loaded task than the parent
-//			oneElemMaxLoaded = this.loadedTasks.clone();
-//			oneElemMaxLoaded.removeAll(this.parent.getLoadedTasks());
-//			for (Task onlyTask : oneElemMaxLoaded) {
-//				currentPlan.appendPickup(onlyTask);
-//			}
-//		}
-////		System.out.println(System.nanoTime() - timeBegin);
-//		return currentPlan;
+//	public void setParentCost(double cost) {
+//		this.costParent = cost;
 //	}
+	
+	public void setParent(State state) {
+		this.parent = state;
+	}
+	
+	/*
+	 * Returns the plan from the initial state to the current state
+	 */
+	public Plan getPlan() {
+//		long timeBegin = System.nanoTime();
+		if (this.parent == null) {
+			return new Plan(this.city);
+		}
+		// Recursive call to parent getPlan() method
+		Plan currentPlan = this.parent.getPlan();
+		
+		TaskSet oneElemMaxLoaded;
+		TaskSet oneElemMaxAvailable = this.parent.getAvailableTasks().clone();
+		oneElemMaxAvailable.removeAll(this.availableTasks);
+		
+		// The current city is a deliver city for the parent node
+		if (oneElemMaxAvailable.size() == 0) {
+			for (City city : this.parent.getCity().pathTo(this.city)) {
+				currentPlan.appendMove(city);
+				System.out.println(city);
+			}
+			// If the current city is a deliver city, then, the parent node has 1 extra loaded task than the current node
+			oneElemMaxLoaded = this.parent.getLoadedTasks().clone();
+			oneElemMaxLoaded.removeAll(this.loadedTasks);
+			for (Task onlyTask : oneElemMaxLoaded) {
+				currentPlan.appendDelivery(onlyTask);
+			}
+		}
+		
+		// The current city is a pickup city for the parent node
+		else {
+			for (City city : this.parent.getCity().pathTo(this.city)) {
+				currentPlan.appendMove(city);
+				System.out.println(city);
+			}
+			// If the current city is a pickup city, then, the current node has 1 extra loaded task than the parent
+			oneElemMaxLoaded = this.loadedTasks.clone();
+			oneElemMaxLoaded.removeAll(this.parent.getLoadedTasks());
+			for (Task onlyTask : oneElemMaxLoaded) {
+				currentPlan.appendPickup(onlyTask);
+			}
+		}
+//		System.out.println(System.nanoTime() - timeBegin);
+		return currentPlan;
+	}
 }
